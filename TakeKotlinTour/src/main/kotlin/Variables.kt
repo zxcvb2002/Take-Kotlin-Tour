1b9ac20cea6b64f0e44426782e192ebb1f13f946
zxcvb2002 <jbyjby3743@naver.com>
2024-03-28 09:39:20 +0900

    [FIX] Variables

--- TakeKotlinTour/src/main/kotlin/Variables.kt
+++ TakeKotlinTour/src/main/kotlin/Variables.kt
@@ -1,22 +1,24 @@
 fun main() {
     variables()
-    practice()
+    practice1()
 }
 
 fun variables() {
     val popcorn = 5
     val hotdog = 7
-    val customers = 10
+    var customers = 10
 
     println(popcorn)
     println(hotdog)
+
+    customers = 8
     println(customers)
 
     println("There are $customers customers")
     println("There are ${customers + 1} customers")
 }
 
-fun practice() {
+fun practice1() {
     val name = "Mary"
     val age = 20
     println("$name is $age years old")
